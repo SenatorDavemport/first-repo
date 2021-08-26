9f160d587b8a849f32c92b0ad64668ed08caf1c8
Senator Davemport <raffaelegalbani@gmail.com>
2021-08-26 20:46:18 +0200

    last commit of the night

--- jj.py
+++ jj.py
@@ -5,3 +5,5 @@ print("making 2nd commit")
 print("ok, maybe this is the 2nd commit, not sure")
 
 print("making commits using VSC")
+
+print("final git, inserting the 3 commands one after the other")
