36fa6f796e43f2cfddc35dda34f22ef19d27caa8
Senator Davemport <raffaelegalbani@gmail.com>
2021-08-26 20:48:36 +0200

    this is the last commit of the night

--- jj.py
+++ jj.py
@@ -7,3 +7,5 @@ print("ok, maybe this is the 2nd commit, not sure")
 print("making commits using VSC")
 
 print("final git, inserting the 3 commands one after the other")
+
+print("i am an idiot, these are the 3 commands: 'git add . / git commit -m 'name of the commit / git push")
